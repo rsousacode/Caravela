b9dfe3d30774ea23e87cd6d00fbc679eaea1361b
Rui Sousa <rsousa@codeliturgy.com>
2026-04-17 04:28:10 +0100

    Teach formatter about Caravela DSL macros

--- .formatter.exs
+++ .formatter.exs
@@ -1,4 +1,13 @@
 # Used by "mix format"
+locals_without_parens = [
+  entity: 2,
+  field: 2,
+  field: 3,
+  relation: 3
+]
+
 [
-  inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"]
+  inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"],
+  locals_without_parens: locals_without_parens,
+  export: [locals_without_parens: locals_without_parens]
 ]
